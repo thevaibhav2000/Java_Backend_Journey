6ed911ce67c9880453744755aa2ae3f09923833b
Vaibhav Upadhyay <upadhyayvaibhav985@gmail.com>
2026-06-22 22:38:30 +0530

    feat: compile and excute first java program

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("I am building my foundation line by line.");
+        System.out.println("Step 3: Java environment is fully operational.");   
+    }
+}
