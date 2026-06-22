4b80d27ef65e3061e9234ec9f25e70a22049d70d
Vaibhav Upadhyay <upadhyayvaibhav985@gmail.com>
2026-06-22 22:50:27 +0530

    feat: implement primitive and refrence data type

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("I am building my foundation line by line.");
-        System.out.println("Step 3: Java environment is fully operational.");   
+     int item_price = 10;
+     double tax_rate = 0.18;
+     boolean is_item_on_sale = true;
+        double tax_amount = item_price * tax_rate;
+        double total_price = item_price + tax_amount;
+
+        String storeMessage = "Thank you for shopping with us!";
+      System.out.println("Availability Status: " + is_item_on_sale);
+        System.out.println("Item Price: $" + item_price);
+        System.out.println("Tax Amount: $" + tax_amount);
+        System.out.println("Total Price: $" + total_price);
+          System.out.println(storeMessage);
     }
 }
