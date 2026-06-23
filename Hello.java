f743b00250334370847645e424f8bcdc4110e83d
Vaibhav Upadhyay <upadhyayvaibhav985@gmail.com>
2026-06-23 21:10:34 +0530

    this is for learning do i able to push it with own or not withour seeing any command

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("This is my first java program i am learning the complete java and thn moving forward with the advacce  java and then complete backend java concpepts");
+    }
+}
