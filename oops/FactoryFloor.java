f117b08c50962c337794801d1b1217684fb394de
Vaibhav Upadhyay <upadhyayvaibhav985@gmail.com>
2026-06-23 22:57:07 +0530

    refactor: implement constructor pattern for optimized object initialization

--- oops/FactoryFloor.java
+++ oops/FactoryFloor.java
@@ -2,21 +2,12 @@ package oops;
 
 public class FactoryFloor {
     public static void main(String[] args) {
-        System.out.println("--- Initializing Factory Floor Asset Manager --- \n");
+        System.out.println("--- Initializing Constructor-Driven Asset Manager --- \n");
 
-        // 1. Create the first object instance using the 'new' keyword
-        SmartComponent component1 = new SmartComponent();
-        component1.componentType = "Microcontroller (MCU)";
-        component1.serialNumber = "MCU-2026-X8";
-        component1.isDefective = false;
+        // We now manufacture and initialize our objects in exactly ONE line each!
+        SmartComponent component1 = new SmartComponent("Microcontroller (MCU)", "MCU-2026-X8", false);
+        SmartComponent component2 = new SmartComponent("LCD Display Panel", "LCD-2026-D4", true);
 
-        // 2. Create a second, completely separate object instance
-        SmartComponent component2 = new SmartComponent();
-        component2.componentType = "LCD Display Panel";
-        component2.serialNumber = "LCD-2026-D4";
-        component2.isDefective = true;
-
-        // 3. Trigger their behaviors
         System.out.println("Inspecting Asset 1:");
         component1.printSpecifications();
 
--- oops/SmartComponent.java
+++ oops/SmartComponent.java
@@ -1,11 +1,20 @@
 package oops;
+
 public class SmartComponent {
-    // 1. Attributes / Properties (State)
+    // 1. Attributes
     String componentType;
     String serialNumber;
     boolean isDefective;
 
-    // 2. A Method belonging to this asset (Behavior)
+    // 2. The Constructor (The Setup Mold)
+    // It accepts parameters and assigns them directly to our class variables
+    public SmartComponent(String type, String serial, boolean defective) {
+        componentType = type;
+        serialNumber = serial;
+        isDefective = defective;
+    }
+
+    // 3. Behavior
     public void printSpecifications() {
         System.out.println("Component Type: " + componentType);
         System.out.println("Serial Reference: " + serialNumber);
